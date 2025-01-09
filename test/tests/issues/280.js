9bbcdccb6a7e5d993575c0d1d1ae6e01b72abd67
Stalgia Grigg <stalgia.grigg@gmail.com>
2025-01-09 15:08:36 -0800

    Update 280.js to use unmodified HTML

--- test/tests/issues/280.js
+++ test/tests/issues/280.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 
 describe.skip('issue 280 Regression: Versions >= v5.3.2 are unable to parse specific link', function () {
 	it('Versions: dev', function () {
-		const html = fs.readFileSync(__dirname + '/../../assets/html/HTML Standard.html', 'utf-8');
+		const html = fs.readFileSync(__dirname + '/../../assets/html/html.spec.whatwg.org.html', 'utf-8');
 		console.log('Fetched HTML. Attempting to parse...');
 		console.time('parseHTMLSpec');
 		const parsedHTML = parse(html);
@@ -15,7 +15,7 @@ describe.skip('issue 280 Regression: Versions >= v5.3.2 are unable to parse spec
 		title.text.should.equal('HTML Standard');
 	});
 	it('Versions: v5.3.1', function () {
-		const html = fs.readFileSync(__dirname + '/../../assets/html/HTML Standard.html', 'utf-8');
+		const html = fs.readFileSync(__dirname + '/../../assets/html/html.spec.whatwg.org.html', 'utf-8');
 		console.log('Fetched HTML. Attempting to parse...');
 		console.time('parseHTMLSpec531');
 		const parsedHTML = parse531(html);
@@ -25,7 +25,7 @@ describe.skip('issue 280 Regression: Versions >= v5.3.2 are unable to parse spec
 		title.text.should.equal('HTML Standard');
 	});
 	it('Versions: v6.1.13', function () {
-		const html = fs.readFileSync(__dirname + '/../../assets/html/HTML Standard.html', 'utf-8');
+		const html = fs.readFileSync(__dirname + '/../../assets/html/html.spec.whatwg.org.html', 'utf-8');
 		console.log('Fetched HTML. Attempting to parse...');
 		console.time('parseHTMLSpec6113');
 		const parsedHTML = parse6113(html);
